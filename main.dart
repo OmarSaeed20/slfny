be9039693332c2e2b2b9af37e82dcb25e47843ef
OmarSaeed20 <110351031+OmarSaeed20@users.noreply.github.com>
2022-09-19 17:35:05 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:slfny/screens/verification_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const VerificationScreen(),
+      initialRoute: '',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const VerificationScreen(),
+        )
+      ],
+    );
+  }
+}
